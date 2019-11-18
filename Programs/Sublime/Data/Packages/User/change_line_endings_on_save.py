e6dc4b6e131d8abdfc30b6058da851dca801504e
D-Pow <D-Pow@users.noreply.github.com>
2019-11-18 11:40:40 -0500

    Add ability to check Preferences.sublime-settings entry before rewriting file's line endings

--- Programs/Sublime/Data/Packages/User/change_line_endings_on_save.py
+++ Programs/Sublime/Data/Packages/User/change_line_endings_on_save.py
@@ -1,9 +1,14 @@
+import sublime
 import sublime_plugin
 
 # Options are: "Windows", "Unix", "CR" (don't use CR)
 PREFERRED_LINE_ENDINGS = "Unix"
 
+user_settings = sublime.load_settings("Preferences.sublime-settings")
+
+change_line_endings_activated = user_settings.get('change_line_endings_on_save')
+
 class SilentlyChangeLineEndingsListener(sublime_plugin.EventListener):
     def on_pre_save(self, view):
-        if view.line_endings() != PREFERRED_LINE_ENDINGS:
+        if change_line_endings_activated and view.line_endings() != PREFERRED_LINE_ENDINGS:
             view.set_line_endings(PREFERRED_LINE_ENDINGS)
