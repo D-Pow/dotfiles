ceec4234d7ca5dd17259edd59f30f8b19c30f253
D-Pow <D-Pow@users.noreply.github.com>
2021-12-15 15:19:33 -0700

    Add JSDoc to util functions; Enhance with body-parsing and origin-extraction

--- mac/Nextdoor/bin/nxtdrCorsApi.mjs
+++ mac/Nextdoor/bin/nxtdrCorsApi.mjs
@@ -7,25 +7,57 @@
 
 import 'isomorphic-fetch';
 
+/**
+ * Network request to a URL through a CORS proxy.
+ *
+ * @param {string} url - URL to use in `fetch()`.
+ * @param {string} [options.method] - HTTP method to use; default is `GET`.
+ * @param {Object} [options.headers] - Headers for  the request; default: `Accept: 'application/json'`, `Content-Type: 'application/json'`, `Cache-Control: 'no-cache'`, `Pragma: 'no-cache'`, `Origin: <url-origin>`, `Referer: <url-origin>`.
+ * @param {any} [options.body] - Body for the request; plain objects, arrays, and functions will be stringified, everything else will be passed as-is.
+ * @param {string} [options.responseFunc='json'] - Function to call on the response to get the body.
+ * @param {RequestInit} [options.otherFetchOptions] - Any other `RequestInit` properties to add that aren't covered above.
+ * @returns {{ response: Response, headers: Object, body: any }} - The `fetch` response object (untainted),
+ *         response headers as an object, and the body of the response after calling `await response[responseFunc]()`.
+ */
 async function nextdoorComCorsRequest(url, {
     method,
     headers,
     body,
     responseFunc = 'json',
+    otherFetchOptions = {},
 } = {}) {
-    const res = await fetch(url, {
+    const { origin } = new URL(url);
+    const corsHostUrl = 'https://anime-atsume.herokuapp.com/corsProxy?url=';
+    const proxiedUrl = corsHostUrl + encodeURIComponent(url);
+
+    const getVarType = variable => Object.prototype.toString.call(variable);
+    const bodyType = getVarType(body);
+    const shouldStringify = (
+        Array.isArray(body)
+        || (bodyType === getVarType({}))
+    );
+    const shouldToString = (
+        (bodyType === getVarType(fetch))
+    );
+
+    const res = await fetch(proxiedUrl, {
         method,
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
             'Cache-Control': 'no-cache',
             Pragma: 'no-cache',
-            Origin: 'https://nextdoor.com',
-            Referer: 'https://nextdoor.com',
+            Origin: origin,
+            Referer: origin,
             // 'x-csrftoken': '',
             ...headers,
         },
-        body: body && JSON.stringify(body),
+        body: shouldStringify
+                ? JSON.stringify(body)
+                : shouldToString
+                    ? body.toString()
+                    : body,
+        ...otherFetchOptions,
     });
 
     const response = res.clone();
@@ -52,6 +84,12 @@ async function nextdoorComCorsRequest(url, {
 }
 
 
+/**
+ * Gets all groups available to the user from the [All groups page]{@link https://nextdoor.com/groups/}
+ *
+ * @param {string} cookie - Cookie from your own login session; if unspecified, retrieval will be attempted from going to the home page, but it will likely fail.
+ * @returns {Object} - The resulting JSON response.
+ */
 async function getGroupsForUser(cookie) {
     if (!cookie) {
         const homePage = await nextdoorComCorsRequest('https://nextdoor.com', { responseFunc: 'text' });
