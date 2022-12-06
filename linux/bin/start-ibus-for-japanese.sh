6b8fff686e4baef84c47004cfa12d7d1c3fbadef
D-Pow <D-Pow@users.noreply.github.com>
2022-12-05 21:07:37 -0700

    Minor: Only start Japanese lang input engine in Linux if not already present

--- linux/bin/start-ibus-for-japanese.sh
+++ linux/bin/start-ibus-for-japanese.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Japanese through iBus
-if [[ -n "$(compgen -c ibus-daemon)" ]]; then
+# Only run the command if it exists and the daemon isn't running already
+if [[ -n "$(compgen -c ibus-daemon)" ]] && [[ -z "$(listprocesses ibus-daemon)" ]]; then
     ibus-daemon &
 fi
