a97291e6b853aee45c7b9b31974ff7d2c503a47f
D-Pow <D-Pow@users.noreply.github.com>
2023-12-13 22:14:47 -0500

    Add script/util to fix Linux volume-change muffling sound instead of correctly (in|de)creasing it
    Also, organize order of custom keybindings for ease of reading

---
+++ linux/bin/changeVolumeForSoundSources.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+changeVolumeForSoundSources() {
+    declare USAGE="[...OPTIONS]
+    Modifies volume for any/all application sources outputting sound.
+    Defaults to changing all by 5%.
+
+    Options:
+        -d <delta>      |   Percentage delta the volume should be changed.
+        -n              |   Negate \`delta\` percentage for decreasing volume.
+                        |   Shorthand for \`-d '-5%'\`.
+        -s <source>     |   Sink input ID(s) from \`pactl list sink-inputs\`.
+                        |   Sink inputs are the sources of sound. Outputs are devices.
+    "
+    declare delta=5
+    declare negate=
+    declare soundSources=()
+
+    declare OPTIND=1
+    while getopts "d:s:n" opt; do
+        case "$opt" in
+            d)
+                delta="$OPTARG"
+                ;;
+            n)
+                negate=true
+                ;;
+            s)
+                soundSources+=("$OPTARG")
+                ;;
+            ?)
+                echo -e
+        esac
+    done
+    shift $(( OPTIND - 1 ))
+
+    declare deltaArg="${negate:+-}${negate:-+}${delta}%"
+    deltaArg="$(echo "$deltaArg" | sed -E 's/^([+-])+/\1/; s/%+$/%/')"
+
+    declare allSoundSources=($(
+        pactl list sink-inputs \
+            | grep -P '^Sink Input' \
+            | grep -P -o --color=never '(?<=#)\d+'
+    ))
+
+    if (( ${#soundSources[@]} == 0 )); then
+        soundSources=("${allSoundSources[@]}")
+    fi
+
+    declare soundSource=
+    for soundSource in ${soundSources[@]}; do
+        pactl set-sink-input-volume $soundSource $deltaArg
+    done
+}
+
+
+
+if [[ "${BASH_SOURCE[0]}" == "${BASH_SOURCE[ ${#BASH_SOURCE[@]} - 1 ]}" ]]; then
+    changeVolumeForSoundSources "$@"
+fi
