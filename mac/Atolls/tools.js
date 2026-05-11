0244631e1b292b68e9e1c4c5215dea7be4219063
D-Pow <D-Pow@users.noreply.github.com>
2026-05-11 09:39:27 +0200

    Add more generic info and tools

--- mac/Atolls/tools.js
+++ mac/Atolls/tools.js
@@ -1,3 +1,21 @@
+#!/usr/bin/env node
+
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
+
+function cmd(cmdStr) {
+    return execSync(cmdStr, {
+        shell: '/opt/homebrew/bin/bash',
+        stdio: 'inherit',
+    })
+        // .toString()
+        // .replace(/\n/g, '');
+}
+
+
+
+async function testGraphQl() {
+    return
 // await (await fetch('https://apollo-router-internal-idknsumb.dev.foundation.gsg.pub/', {
 await (await fetch('http://localhost:5001', {
     method: 'POST',
@@ -33,3 +51,56 @@ await (await fetch('http://localhost:5001', {
         },
     }),
 })).json();
+}
+
+
+
+const igraalPolandInfo = {
+    clientId: 'f9f4557825c74791ac01dc5c72ef7aef',
+    memberId: '7aa698b5-6ae5-4047-af1a-471b5eb6c81f',
+    userId: '7aa698b5-6ae5-4047-af1a-471b5eb6c81f',
+};
+
+const igraalGermanyInfo = {
+    clientId: 'c235ee8b371f4e8b8e392330ea359875',
+    memberId: '510fcd50-890d-465d-890e-dcc6d9f53996',
+};
+
+const igraalFranceInfo = {
+    clientId: 'a444a51901d85f3d9eb84ada70cef7f3',
+    memberId: '1a43024a-12de-4586-accc-94814ccc7793',
+    userId: '1a43024a-12de-4586-accc-94814ccc7793',
+};
+
+const locale = 'PL';
+
+const topupAccountBody = {
+    amount: {
+        amount: '100',
+        currency: locale === 'PL' ? 'PLN' : 'EUR',
+    },
+    clientId: (locale === 'PL' ? igraalPolandInfo : igraalGermanyInfo).clientId,
+    memberId: (locale === 'PL' ? igraalPolandInfo : igraalGermanyInfo).memberId,
+    metadata: {},
+    referenceDate: new Date().toISOString(),
+    referenceId: randomUUID(),
+    referenceMessage: 'Manual top-up',
+    source: 'acquisition_welcome_bonus',
+    // threadId: '',
+};
+
+const message = {
+    id: randomUUID(),
+    name: 'command.ledger.request-and-approve-deposit.v4',
+    source: 'ssp-ledger-service-app',
+    timestamp: new Date().toISOString(),
+    type: 'COMMAND',
+    body: topupAccountBody,
+};
+
+
+function topupAccount() {
+    return cmd(`source $HOME/.profile && sendSqsMessageInLocalstack '${JSON.stringify(message)}'`);
+}
+
+topupAccount();
